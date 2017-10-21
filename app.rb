f6364ea919550189e2c4adde13b770d9dfe9f688
kur0 <over.y31.bsk@gmail.com>
2017-10-22 00:29:06 +0900

    redis入れた

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'mysql2'
 require 'mysql2-cs-bind'
 require 'erubis'
+require 'logger'
+require 'redis'
 
 module Ishocon1
   class AuthenticationError < StandardError; end
@@ -14,37 +16,41 @@ class Ishocon1::WebApp < Sinatra::Base
   set :erb, escape_html: true
   set :public_folder, File.expand_path('../public', __FILE__)
   set :protection, true
+  Log = Logger.new('dev.log')
 
   helpers do
     def config
       @config ||= {
-        db: {
-          host: ENV['ISHOCON1_DB_HOST'] || 'localhost',
-          port: ENV['ISHOCON1_DB_PORT'] && ENV['ISHOCON1_DB_PORT'].to_i,
-          username: ENV['ISHOCON1_DB_USER'] || 'ishocon',
-          password: ENV['ISHOCON1_DB_PASSWORD'] || 'ishocon',
-          database: ENV['ISHOCON1_DB_NAME'] || 'ishocon1'
-        }
+          db: {
+              host: ENV['ISHOCON1_DB_HOST'] || 'localhost',
+              port: ENV['ISHOCON1_DB_PORT'] && ENV['ISHOCON1_DB_PORT'].to_i,
+              username: ENV['ISHOCON1_DB_USER'] || 'ishocon',
+              password: ENV['ISHOCON1_DB_PASSWORD'] || 'ishocon',
+              database: ENV['ISHOCON1_DB_NAME'] || 'ishocon1'
+          }
       }
+    end
+
+    def redis
+      return Thread.current[:ishocon1_redis] if Thread.current[:ishocon1_redis]
+      client = Redis.new(:path => '/tmp/redis.sock')
+      Thread.current[:ishocon1_redis] = client
+      client
     end
 
     def db
       return Thread.current[:ishocon1_db] if Thread.current[:ishocon1_db]
       client = Mysql2::Client.new(
-        host: config[:db][:host],
-        port: config[:db][:port],
-        username: config[:db][:username],
-        password: config[:db][:password],
-        database: config[:db][:database],
-        reconnect: true
+          host: config[:db][:host],
+          port: config[:db][:port],
+          username: config[:db][:username],
+          password: config[:db][:password],
+          database: config[:db][:database],
+          reconnect: true
       )
       client.query_options.merge!(symbolize_keys: true)
       Thread.current[:ishocon1_db] = client
       client
-    end
-
-    def time_now_db
-      Time.now - 9 * 60 * 60
     end
 
     def authenticate(email, password)
@@ -54,7 +60,7 @@ class Ishocon1::WebApp < Sinatra::Base
     end
 
     def authenticated!
-      fail Ishocon1::PermissionDenied unless current_user
+      fail Ishocon1::PermissionDenied unless session[:user_id]
     end
 
     def current_user
@@ -62,24 +68,32 @@ class Ishocon1::WebApp < Sinatra::Base
     end
 
     def update_last_login(user_id)
-      db.xquery('UPDATE users SET last_login = ? WHERE id = ?', time_now_db, user_id)
+      db.xquery('UPDATE users SET last_login = ? WHERE id = ?', Time.now, user_id)
     end
 
     def buy_product(product_id, user_id)
       db.xquery('INSERT INTO histories (product_id, user_id, created_at) VALUES (?, ?, ?)', \
-        product_id, user_id, time_now_db)
+        product_id, user_id, Time.now)
     end
 
     def already_bought?(product_id)
-      return false unless current_user
+      return false unless session[:user_id]
       count = db.xquery('SELECT count(*) as count FROM histories WHERE product_id = ? AND user_id = ?', \
-                        product_id, current_user[:id]).first[:count]
+                        product_id, session[:user_id]).first[:count]
       count > 0
     end
 
     def create_comment(product_id, user_id, content)
-      db.xquery('INSERT INTO comments (product_id, user_id, content, created_at) VALUES (?, ?, ?, ?)', \
-        product_id, user_id, content, time_now_db)
+      # コメント総件数更新
+      redis.incr(product_id.to_s+'cnt')
+
+      # コメント登録
+      user_name = redis.get(user_id.to_s+'nm')
+      key = product_id.to_s+'c'
+      redis.rpop(key) if redis.LLEN(key) == 5
+      redis.lpush(key, user_name+',,'+content[0..25])
+      #db.xquery('INSERT INTO comments (product_id, user_id, content, created_at) VALUES (?, ?, ?, ?)', \
+      #  product_id, user_id, content, Time.now)
     end
   end
 
@@ -99,43 +113,70 @@ class Ishocon1::WebApp < Sinatra::Base
 
   post '/login' do
     authenticate(params['email'], params['password'])
-    update_last_login(current_user[:id])
-    redirect '/'
+
+    # index と一緒
+    products = db.xquery("SELECT * FROM products_0 order by id desc")
+    #product_ids = result.map { |r| r[:id] }
+    #query = <<SQL
+    #SELECT *
+    #FROM products_short
+    #WHERE id in (?)
+    #ORDER BY id DESC
+    #SQL
+    #products = db.xquery(query, product_ids)
+
+    erb :index, locals: { products: products }
   end
 
   get '/logout' do
     session[:user_id] = nil
     session.clear
-    redirect '/login'
+    erb :login, layout: false, locals: { message: 'ECサイトで爆買いしよう！！！！' }
   end
 
   get '/' do
     page = params[:page].to_i || 0
-    products = db.xquery("SELECT * FROM products ORDER BY id DESC LIMIT 50 OFFSET #{page * 50}")
-    cmt_query = <<SQL
-SELECT *
-FROM comments as c
-INNER JOIN users as u
-ON c.user_id = u.id
-WHERE c.product_id = ?
-ORDER BY c.created_at DESC
-LIMIT 5
-SQL
-    cmt_count_query = 'SELECT count(*) as count FROM comments WHERE product_id = ?'
 
-    erb :index, locals: { products: products, cmt_query: cmt_query, cmt_count_query: cmt_count_query }
+    products = db.xquery("SELECT * FROM products_#{page} ORDER BY id DESC")
+    #product_ids = result.map { |r| r[:id] }
+    #query = <<SQL
+    #SELECT *
+    #FROM products_short
+    #WHERE id in (?)
+    #ORDER BY id DESC
+    #SQL
+    #products = db.xquery(query, product_ids)
+
+    #cmt_query = <<SQL
+    #SELECT LEFT(content, 26), user_id
+    #FROM comments
+    #WHERE product_id = ?
+    #ORDER BY created_at DESC
+    #LIMIT 5
+    #SQL
+    #cmt_count_query = 'SELECT count(*) as count FROM comments WHERE product_id = ?'
+
+    erb :index, locals: { products: products }
   end
 
   get '/users/:user_id' do
+    query = <<SQL
+SELECT id
+FROM histories
+WHERE user_id = ?
+SQL
+    result = db.xquery(query, params[:user_id])
+    his_ids = result.map { |r| r[:id] }
+
     products_query = <<SQL
-SELECT p.id, p.name, p.description, p.image_path, p.price, h.created_at
+SELECT p.*, h.created_at
 FROM histories as h
-LEFT OUTER JOIN products as p
+LEFT OUTER JOIN products_short as p
 ON h.product_id = p.id
-WHERE h.user_id = ?
+WHERE h.id in (?)
 ORDER BY h.id DESC
 SQL
-    products = db.xquery(products_query, params[:user_id])
+    products = db.xquery(products_query, his_ids)
 
     total_pay = 0
     products.each do |product|
@@ -148,20 +189,19 @@ SQL
 
   get '/products/:product_id' do
     product = db.xquery('SELECT * FROM products WHERE id = ?', params[:product_id]).first
-    comments = db.xquery('SELECT * FROM comments WHERE product_id = ?', params[:product_id])
-    erb :product, locals: { product: product, comments: comments }
+    erb :product, locals: { product: product }
   end
 
   post '/products/buy/:product_id' do
     authenticated!
-    buy_product(params[:product_id], current_user[:id])
-    redirect "/users/#{current_user[:id]}"
+    buy_product(params[:product_id], session[:user_id])
+    redirect "/users/#{session[:user_id]}"
   end
 
   post '/comments/:product_id' do
     authenticated!
-    create_comment(params[:product_id], current_user[:id], params[:content])
-    redirect "/users/#{current_user[:id]}"
+    create_comment(params[:product_id], session[:user_id], params[:content])
+    redirect "/users/#{session[:user_id]}"
   end
 
   get '/initialize' do
@@ -169,6 +209,19 @@ SQL
     db.query('DELETE FROM products WHERE id > 10000')
     db.query('DELETE FROM comments WHERE id > 200000')
     db.query('DELETE FROM histories WHERE id > 500000')
+    redis.flushall
+    users = db.query('select * from users')
+    users.each do |user|
+      redis.set(user[:id].to_s+'nm', user[:name])
+    end
+    comments = db.query('select * from comments order by created_at limit 100000')
+    comments.each do |c|
+      create_comment(c[:product_id], c[:user_id], c[:content])
+    end
+    products = db.query('select id from products')
+    products.each do |pro|
+      redis.set(pro[:id].to_s+'cnt', 20)
+    end
     "Finish"
   end
 end
